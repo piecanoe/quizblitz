73fa0f42c3f64407c51b6ed979e491ea427ba818
piecanoe <karenkaydu@gmail.com>
2024-03-17 16:19:50 -0700

    Add ScorePage

---
+++ src/views/ScorePage.jsx
@@ -0,0 +1,10 @@
+const ScorePage = ({ score }) => {
+  return (
+    <div className="score-page">
+      <h1>Score Page</h1>
+      <p>Your score: {score}%</p>
+    </div>
+  );
+};
+
+export default ScorePage;
